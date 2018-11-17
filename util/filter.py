1d4e2501fdc9cca04306231c6d9c6d4566442629
paraz <mail2paras.s@gmail.com>
2018-11-17 16:18:46 +0530

    general code

--- util/filter.py
+++ util/filter.py
@@ -6,6 +6,7 @@ def image_otsu(img):
     ret, thr = cv2.threshold(img, 0, 255, cv2.THRESH_OTSU)
     return thr
 
+
 def image_resize(img, size):
     resized_image = cv2.resize(img, (size[0], size[1]))
     return resized_image
@@ -18,5 +19,5 @@ def save_image(save_path, img):
 # test code
 if __name__ == "__main__":
     img = image_otsu("img.jpg")
-    img  = image_resize(img, (400, 200))
+    img = image_resize(img, (400, 200))
     save_image("processed.jpg", img)
