7f085a7f4c27b115bd63b3e98b3c0a0cf723bf43
paraz <mail2paras.s@gmail.com>
2018-11-17 16:17:53 +0530

    general code

--- util/filter.py
+++ util/filter.py
@@ -1,6 +1,22 @@
 import cv2
-a = cv2.imread('img.jpg',0) 
-ret,thr = cv2.threshold(a, 0, 255, cv2.THRESH_OTSU)
-resized_image = cv2.resize(thr, (600,600))
-cv2.imwrite('processed.jpg',resized_image)
-cv2.destroyAllWindows()
+
+
+def image_otsu(img):
+    img = cv2.imread(img, 0)
+    ret, thr = cv2.threshold(img, 0, 255, cv2.THRESH_OTSU)
+    return thr
+
+def image_resize(img, size):
+    resized_image = cv2.resize(img, (size[0], size[1]))
+    return resized_image
+
+
+def save_image(save_path, img):
+    cv2.imwrite(save_path, img)
+
+
+# test code
+if __name__ == "__main__":
+    img = image_otsu("img.jpg")
+    img  = image_resize(img, (400, 200))
+    save_image("processed.jpg", img)
